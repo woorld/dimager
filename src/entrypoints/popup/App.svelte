f3ef3817db9791fce09ce0da8eb2ac49cd8e0eb6
woorld <17719812+woorld@users.noreply.github.com>
2025-10-08 21:41:28 +0900

    対象のタブが存在しない場合のエラー表示を実装

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+  let isNoImageTabErrorVisible = $state(false);
+
   const downloadImage = async () => {
     const tabs = await browser.tabs.query({
       url: 'https://cdn.discordapp.com/attachments/*' // TODO: URL共通化
     });
 
     if (tabs.length <= 0) {
+      isNoImageTabErrorVisible = true;
       return;
     }
+    isNoImageTabErrorVisible = false;
 
     const closeTabIds: number[] = [];
 
@@ -25,5 +29,14 @@
 </script>
 
 <main>
+  {#if isNoImageTabErrorVisible}
+    <p class="red">Discordの画像タブがありません</p>
+  {/if}
   <button onclick={downloadImage} >一括DLしてタブを閉じる</button>
 </main>
+
+<style>
+  .red {
+    color: #ee3333;
+  }
+</style>
